b0c79f66e62c086b273f9839b411cf526063c31d
Kamil Ogórek <kamil.ogorek@gmail.com>
2019-09-26 11:05:03 +0200

    add real browsers list

--- browsers.js
+++ browsers.js
@@ -1,21 +1,94 @@
 module.exports = [
   {
-    browserName: "IE",
-    browser_version: "10.0",
+    browser: "Chrome",
+    browser_version: "latest",
     os: "Windows",
-    os_version: "7"
+    os_version: "10",
   },
-
-  {
-    browserName: "IE",
-    browser_version: "11.0",
-    os: "Windows",
-    os_version: "10"
-  },
-  {
-    browserName: "Edge",
-    browser_version: "18.0",
-    os: "Windows",
-    os_version: "10"
-  }
+  // {
+  //   browser: "Firefox",
+  //   browser_version: "latest",
+  //   os: "Windows",
+  //   os_version: "10",
+  // },
+  // {
+  //   browser: "Safari",
+  //   browser_version: "latest",
+  //   os: "OS X",
+  //   os_version: "Mojave",
+  // },
+  // {
+  //   browser: "Edge",
+  //   browser_version: "latest",
+  //   os: "Windows",
+  //   os_version: "10",
+  // },
+  // {
+  //   browser: "IE",
+  //   browser_version: "11.0",
+  //   os: "Windows",
+  //   os_version: "10",
+  // },
+  // {
+  //   browser: "IE",
+  //   browser_version: "10.0",
+  //   os: "Windows",
+  //   os_version: "8",
+  // },
+  // {
+  //   browser: "Mobile Safari",
+  //   device: "iPhone 8",
+  //   os: "ios",
+  //   os_version: "12.1",
+  //   real_mobile: true,
+  // },
+  // {
+  //   browser: "Mobile Safari",
+  //   device: "iPhone 6",
+  //   os: "ios",
+  //   os_version: "11.4",
+  //   real_mobile: true,
+  // },
+  // {
+  //   browser: "Android",
+  //   device: "Samsung Galaxy S10",
+  //   os: "android",
+  //   os_version: "9.0",
+  //   real_mobile: true,
+  // },
+  // {
+  //   browser: "Android",
+  //   device: "Samsung Galaxy S9",
+  //   os: "android",
+  //   os_version: "8.0",
+  //   real_mobile: true,
+  // },
+  // {
+  //   browser: "Android",
+  //   device: "Samsung Galaxy S8",
+  //   os: "android",
+  //   os_version: "7.0",
+  //   real_mobile: true,
+  // },
+  // {
+  //   browser: "Android",
+  //   device: "Samsung Galaxy S7",
+  //   os: "android",
+  //   os_version: "6.0",
+  //   real_mobile: true,
+  // },
+  // {
+  //   browser: "Android",
+  //   device: "Google Nexus 9",
+  //   os: "android",
+  //   os_version: "5.1",
+  //   real_mobile: true,
+  // },
+  // {
+  //   browser: "Android",
+  //   device: "Google Nexus 5",
+  //   os: "android",
+  //   os_version: "4.4",
+  //   real_mobile: true,
+  // },
 ];
